ace8f1d56d45a6705fdb7c0dc2876dfb86eda7e6
Elie RASOANAIVO <erasoanaivo-ext@agirc-arrco.fr>
2024-01-19 06:31:05 +0100

    feat(formation): observable principle

--- formation-v1/src/app/callback/ClientCallBack.ts
+++ formation-v1/src/app/callback/ClientCallBack.ts
@@ -1,19 +1,26 @@
-import {callBackFunctionAsync, callBackFunctionSync} from "./call-back-function";
+import {CallBackFunction} from "./call-back-function";
 
 export class ClientCallBack {
-    
+
     public static syncCallBack(): void {
-        callBackFunctionSync((data: string): void => {
+        CallBackFunction.callBackFunctionSync((data: string): void => {
             // todo some treatment
             console.error("client sync callBack", data);
         });
     }
-    
+
     public static asyncCallBack(): void {
-        callBackFunctionAsync((data: string): void => {
+        CallBackFunction.callBackFunctionAsync((data: string): void => {
             // todo some treatment
             console.error("client async callBack", data);
         });
     }
-    
+
+    public static asyncCallBackSubscribe(): void {
+        CallBackFunction.callBackFunctionAsyncSubscribe((data: string): void => {
+            // todo some treatment
+            console.error("client async subscribe callBack", data);
+        });
+    }
+
 }
--- formation-v1/src/app/callback/call-back-function.ts
+++ formation-v1/src/app/callback/call-back-function.ts
@@ -1,28 +1,45 @@
-export function callBackFunctionSync(param: (calbackParam: string) => void): void {
-    const megaCalcul: string = Math.random().toFixed(2);
-    console.error("Sync before callback", megaCalcul);
-    // .... others instructions
+export class CallBackFunction {
+    private static callBackObs: (calbackParam: string) => void;
 
-    // then call back function in parameter
-    param(megaCalcul);
+    public static callBackFunctionSync(param: (calbackParam: string) => void): void {
+        const megaCalcul: string = Math.random().toFixed(2);
+        console.error("Sync before callback", megaCalcul);
+        // .... others instructions
 
-    console.error("Sync  after callback", megaCalcul);
-}
-
-
-export function callBackFunctionAsync(param: (calbackParam: string) => void): void {
-    let megaCalcul: string = Math.random().toFixed(2);
-    console.error("Async before callback", megaCalcul);
-    // .... others instructions
-
-    // Async treatment
-    setTimeout((): void => {
-        // update megaCalcul
-        megaCalcul = Math.random().toFixed(4);
-        
         // then call back function in parameter
         param(megaCalcul);
-    }, 5000);
+
+        console.error("Sync  after callback", megaCalcul);
+    }
+
+
+    public static callBackFunctionAsync(param: (calbackParam: string) => void): void {
+        let megaCalcul: string = Math.random().toFixed(2);
+        console.error("Async before callback", megaCalcul);
+        // .... others instructions
+
+        // Async treatment
+        setTimeout((): void => {
+            // update megaCalcul
+            megaCalcul = Math.random().toFixed(4);
+
+            // then call back function in parameter
+            param(megaCalcul);
+        }, 5000);
+
+        console.error("Async after callback", megaCalcul);
+    }
+
+
+    public static callBackFunctionAsyncSubscribe(param: (calbackParam: string) => void): void {
+        console.error("Subscribe Async");
+        CallBackFunction.callBackObs = param;
+    }
     
-    console.error("Async after callback", megaCalcul);
+    public static emitValeur(): void {
+        console.error("emit valeur");
+        const megaCalcul: string = Math.random().toFixed(5);
+        CallBackFunction.callBackObs(megaCalcul)
+    }
+    
 }
